d0e302e1c270147aec74c2cb4b06263433daf3ff
Javier Fray <jfray.wx@gmail.com>
2024-03-16 00:20:47 -0500

    ADD i18dependencies and configuration

---
+++ i18n.config.ts
@@ -0,0 +1,15 @@
+export default defineI18nConfig(() => ({
+    legacy: false,
+    messages: {
+        en: {
+            homePage: {
+
+            }
+        },
+        es: {
+            homePage: {
+
+            }
+        },
+    },
+}));
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/i18n'
+  ],
+  i18n: {
+    locales: ["en", "es"],
+    strategy: "prefix",
+    defaultLocale: "en",
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'lang',
+      redirectOn: 'all',
+      alwaysRedirect: true,
+    },
+    vueI18n: "./i18n.config.ts",
+  },
 })
